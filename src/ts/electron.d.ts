be52fd1179ca6a8890a3fdbeb858d12b270d5114
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-10 15:39:35 +0200

    Update electron TS declaration

--- src/ts/electron.d.ts
+++ src/ts/electron.d.ts
@@ -1,9 +1,10 @@
-import { Preferences } from '../types/types';
+import { Inventory, Preferences } from '../types/types';
 interface Window {
     electron: {
         requestPrefsJson: () => string;
         requestDataJson: () => string;
         wipeOvs: () => 0 | 1;
-        changeOvsSettings: (arg: Preferences) => 0 | 1
+        changeOvsSettings: (arg: Preferences) => 0 | 1;
+        writeDataJson: (arg: Inventory) => 0 | 1;
     };
 }
